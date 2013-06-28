3ad13633b532cc86dbc98bb80eef1d559dcf741f
csguth <csguth@gmail.com>
2013-06-28 17:37:00 -0300

    Doesn't need to 'make clean'

--- src/runTimer.sh
+++ src/runTimer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export ISPD_ROOT=/home/chrystian/Documents/Cientifico/ISPD/ispd_files/ispd2013
-export ISPD_BENCHMARK=simple
+export ISPD_BENCHMARK=usb_phy
 
 echo "ISPD_ROOT is " $ISPD_ROOT
 echo "ISPD_BENCHMARK is " $ISPD_BENCHMARK
 
-make clean && make && ./TimingAnalysis $ISPD_ROOT $ISPD_BENCHMARK
+make && ./TimingAnalysis $ISPD_ROOT $ISPD_BENCHMARK
