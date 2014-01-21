d0474d5f85ccee2a83c320ec2b18a2b93dfba668
csguth <csguth@gmail.com>
2014-01-21 15:10:16 -0200

    Autotools!

--- src/RunTimer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export ISPD_BENCHMARK=$1
-export ISPD_ROOT=~/Documents/Cientifico/ISPD/ispd_files/ispd$2
-
-echo "ISPD_ROOT is " $ISPD_ROOT
-echo "ISPD_BENCHMARK is " $ISPD_BENCHMARK
-
-make && ./TimingAnalysis $ISPD_ROOT $ISPD_BENCHMARK
